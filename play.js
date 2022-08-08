f839f800e36e3c0b2e0a55c27427ff96cb875750
Ruanruanx <zxin7@uwo.ca>
2022-08-08 20:38:25 +0000

    Input module

--- play.js
+++ play.js
@@ -1,20 +1,6 @@
-const connect = require('./client.js');
-const stdin = process.stdin;
-
-const setupInput = function() {
-  stdin.on('data', handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-const handleUserInput = function(key) {
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-
-}
+const {connect} = require('./client.js');
+const {setupInput} = require('./input.js')
+console.log("Connecting ...");
+connect();
 
 setupInput();

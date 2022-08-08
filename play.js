058e17f8de7200b9ac48b0d384d34fef4f0c689f
Ruanruanx <zxin7@uwo.ca>
2022-08-08 20:13:21 +0000

    setup input

--- play.js
+++ play.js
@@ -1,1 +1,20 @@
 const connect = require('./client.js');
+const stdin = process.stdin;
+
+const setupInput = function() {
+  stdin.on('data', handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+const handleUserInput = function(key) {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+
+}
+
+setupInput();

199a08f0d898b6195fd700a7f73495ebf3b09c83
Ruanruanx <zxin7@uwo.ca>
2022-08-09 19:24:03 +0000

    add move function to snake

---
+++ input.js
@@ -0,0 +1,37 @@
+const { connect } = require("http2");
+
+const stdin = process.stdin;
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  stdin.on('data', handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    if (key === 'w') {
+      console.log(key);
+      connection.write("Move: up")
+    }
+    if (key === 'a') {
+      console.log(key);
+      connection.write("Move: left")
+    }
+    if(key === 's'){
+      connection.write("Move: down")
+    }
+    if(key ==='d'){
+      connection.write("Move: right")
+    }
+  })
+}
+
+module.exports = { setupInput };
